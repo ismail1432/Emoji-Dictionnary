1db9cd1b4c650246b632bc204c7abac17a174250
smaine M <contact@smaine.me>
2017-09-24 09:40:49 +0100

    Test audio ok 👍

--- SecondViewController.swift
+++ SecondViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2017 Smaine Milianni. All rights reserved.
 //
 
+import AVFoundation
 import UIKit
 
 class SecondViewController: UIViewController {
@@ -13,21 +14,39 @@ class SecondViewController: UIViewController {
     var emojiSelect = Emoji();
     var emojiDescribe = "Describe";
     
+    var audioPlayer: AVAudioPlayer?
+    
     
     @IBOutlet weak var category: UILabel!
     @IBOutlet weak var emojiDescription: UILabel!
     @IBOutlet weak var emojiToShow: UILabel!
     
-     var emojisArrayDescription = ["Super Lunettes", "Chenille-ZER", "1 boug Mickey", "hand in you face", "cAnaRdo !!"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         emojiToShow.text = emojiSelect.emojiString;
         emojiDescription.text = emojiSelect.description;
         category.text = emojiSelect.translate.uppercased();
+        
+        let audioFileName = "idrissAbkar-Coran-6"
+        let audioFileURL = ""
+        if let audioFileURL = Bundle.main.url(forResource: audioFileName, withExtension: "mp3", subdirectory: "audio") {
+            print(audioFileURL)
+        }
+        
+        let path = Bundle.main.path(forResource: audioFileURL, ofType:"mp3")!
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer?.play()
+        } catch {
+            // couldn't load file :(
+        }
     }
 
-    
+
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
